309b215b1565ac2957b13c4469e1c4815d0acf71
Seba-Barco <ggsbarco@gmail.com>
2024-01-25 01:36:08 -0300

    Refactor: Created utils.js for shared algorithm functionality

--- script.js
+++ script.js
@@ -58,24 +58,3 @@ function index(i, j) {
   }
   return j + i * columns;
 }
-
-function removeWalls(a, b) {
-  var horizontal = a.j - b.j;
-  var vertical = a.i - b.i;
-  if (horizontal == -1) {
-    a.walls[right] = false;
-    b.walls[left] = false;
-  }
-  else if (horizontal == 1) {
-    a.walls[left] = false;
-    b.walls[right] = false;
-  }
-  if (vertical == -1) {
-    a.walls[bottom] = false;
-    b.walls[top] = false;
-  }
-  else if (vertical == 1) {
-    a.walls[top] = false;
-    b.walls[bottom] = false;
-  }
-}
