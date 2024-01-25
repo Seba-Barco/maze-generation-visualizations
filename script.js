97af2f34248ad097625be3b40f2f521a8674a938
Seba-Barco <ggsbarco@gmail.com>
2024-01-25 01:24:47 -0300

    Refactor: Extract Cell class into a separate cell.js module

--- script.js
+++ script.js
@@ -4,9 +4,6 @@
 // Slider
 var columns = 50,
   rows = 50;
-// The cellSize variable represents a practical aspect related to the visualization
-// of the grid on the canvas.
-// Slider
 var cellSize = 18;
 var grid = [];
 var current;
@@ -54,88 +51,6 @@ function draw() {
   }
 }
 
-// Constructor function
-function Cell(i, j) {
-  // Vertical position
-  this.i = i;
-  // Horizontal position
-  this.j = j;
-  // Boolean values to enable/disable the walls of each cell in the grid
-  this.walls = [true, true, true, true];
-  this.visited = false;
-  this.inStack = false;
-
-  this.checkNeighbors = function () {
-    var neighbors = [
-      // top
-      grid[index(i - 1, j)],
-      // right
-      grid[index(i, j + 1)],
-      // bottom
-      grid[index(i + 1, j)],
-      // left
-      grid[index(i, j - 1)],
-    ];
-
-    var notVisited = [];
-
-    for (k = 0; k < neighbors.length; k++) {
-      if (neighbors[k] && !neighbors[k].visited) {
-        notVisited.push(neighbors[k]);
-      }
-    }
-
-    if (notVisited.length > 0) {
-      var r = floor(random(0, notVisited.length));
-      return notVisited[r];
-    } else {
-      return undefined;
-    }
-  };
-
-  // Rendering logic
-  this.show = function () {
-    var x = this.j * cellSize;
-    var y = this.i * cellSize;
-    stroke(255);
-
-    // Draw the border lines for the cells
-    if (this.walls[top]) {
-      line(x, y, x + cellSize, y);
-    }
-    if (this.walls[right]) {
-      line(x + cellSize, y, x + cellSize, y + cellSize);
-    }
-    if (this.walls[bottom]) {
-      line(x, y + cellSize, x + cellSize, y + cellSize);
-    }
-    if (this.walls[left]) {
-      line(x, y, x, y + cellSize);
-    }
-
-    // paint the visited cells
-    if (this.inStack) {
-      noStroke();
-      fill(255, 255, 255, 100);
-      rect(x, y, cellSize, cellSize);
-    }
-    else if(this.visited){
-      noStroke();
-      fill(19, 196, 163, 100);
-      rect(x, y, cellSize, cellSize);
-    }
-  };
-
-  // Highlight the current cell
-  this.highlight = function () {
-    var x = this.j * cellSize;
-    var y = this.i * cellSize;
-    noStroke();
-    fill(0, 0, 255, 100);
-    rect(x, y, cellSize, cellSize);
-  };
-}
-
 function index(i, j) {
   // Invalid cases
   if (i < 0 || j < 0 || i > rows - 1 || j > columns - 1) {
@@ -151,7 +66,7 @@ function removeWalls(a, b) {
     a.walls[right] = false;
     b.walls[left] = false;
   }
-  if (horizontal == 1) {
+  else if (horizontal == 1) {
     a.walls[left] = false;
     b.walls[right] = false;
   }
@@ -159,7 +74,7 @@ function removeWalls(a, b) {
     a.walls[bottom] = false;
     b.walls[top] = false;
   }
-  if (vertical == 1) {
+  else if (vertical == 1) {
     a.walls[top] = false;
     b.walls[bottom] = false;
   }
